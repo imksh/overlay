501965815ed9d52d1877ffdfff5a468bfb2e4860
Karan Sharma <karan03945@gmail.com>
2025-10-24 22:55:53 +0530

    innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="relative w-full h-screen">
+      <div className="absolute bottom-0 py-5 px-10 bg-red-700">
+        <h2>Hello</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
